89cb6e34c419d7089549bb07c567e9412d413cc3
vgarciac82 <104453051+vgarciac82@users.noreply.github.com>
2023-02-05 09:41:25 -0600

    Validacion de rutas por JWT y OAuth

--- app/routers/user.py
+++ app/routers/user.py
@@ -4,6 +4,7 @@ from app.db.database import get_db
 from sqlalchemy.orm import Session
 from typing import List
 from app.repository import User_Repository
+from app.oauth import get_current_user
 
 router = APIRouter(
     prefix="/user", 
@@ -12,17 +13,16 @@ router = APIRouter(
  
 @router.post('/crear_usuario',status_code=status.HTTP_201_CREATED)
 def crear_usuario(user:User,db:Session = Depends(get_db)):
-    
     User_Repository.crear_usuario(user,db)
     return {"respuesta":"Usuario creado con exito"}
     
 
-@router.get("/",response_model=List[ShowUser],status_code=200)
-def listar_usuarios(db:Session = Depends(get_db)):
+@router.get('/',response_model=List[ShowUser],status_code=status.HTTP_200_OK)
+def obtener_usuarios(db:Session = Depends(get_db),current_user: User = Depends(get_current_user)):
     data = User_Repository.listar_usuarios(db)
-    return data;
+    return data
 
-@router.get("/{user_id}", response_model=ShowUser)
+@router.get("/{user_id}", response_model=ShowUser )
 def obtener_usuario(user_id:str, db:Session = Depends(get_db)):
     usuario = User_Repository.obtener_usuario(user_id, db)
     if not usuario:
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
+from typing import Union
 
 #User Model
 class User(BaseModel):
@@ -36,3 +37,11 @@ class Login(BaseModel):
     username:str
     password:str
     
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
